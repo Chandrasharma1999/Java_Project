678c507919b391211cf35e4dbe4e5859f85df921
Chandra Swarup Sharma <chandra.1999sharma@gmail.com>
2024-12-01 10:27:19 +0530

    Add multply option in repo

---
+++ Java_Project/src/PrintB.java
@@ -0,0 +1,13 @@
+
+public class PrintB {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a = 10;
+		int b = 20;
+		int d = a*b;
+		System.out.println("Multiply of a and b is:- "+d);
+	
+	}
+
+}
