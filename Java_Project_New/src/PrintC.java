21904de7cb00838c76ee61a8ba96e07f45618866
Chandra Swarup Sharma <chandra.1999sharma@gmail.com>
2024-12-01 11:50:09 +0530

    Initial Commit

---
+++ Java_Project_New/src/PrintC.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class PrintC {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		System.out.print("Enter the No. of Natual number:- ");
+		int n = sc.nextInt();
+		int i;
+		for(i = 1;i<=n;i++) {
+			System.out.println(i);
+			
+		}
+
+	}
+
+}
